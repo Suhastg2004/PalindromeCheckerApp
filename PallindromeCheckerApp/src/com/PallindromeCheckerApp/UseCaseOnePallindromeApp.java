ace560bc4585b5f8024b39e26b536fefd30275da
Suhastg2004 <suhastg263399@gmail.com>
2026-03-03 15:57:37 +0530

    Coded UC1 to display a welcome message

---
+++ PallindromeCheckerApp/src/com/PallindromeCheckerApp/UseCaseOnePallindromeApp.java
@@ -0,0 +1,10 @@
+//author @Suhas T G
+//version 1.0
+
+package com.PallindromeCheckerApp;
+
+public class UseCaseOnePallindromeApp {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Pallindrome Checker Management System");
+	}
+}
